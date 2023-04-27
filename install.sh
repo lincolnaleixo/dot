3af5660f4005c2fee88c156053bc348db330cc16
lincoln <sleeved.quills.0u@icloud.com>
2023-04-27 13:57:09 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt install git zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/powerlevel10k
