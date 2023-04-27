5dc51769d3406959c0c9ed7bce83cea3c24c0b08
lincoln <sleeved.quills.0u@icloud.com>
2023-04-27 14:05:13 +0200

    adding p10k config

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo apt install git zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
